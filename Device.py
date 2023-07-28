f517d1fae0dec3b2741a304d89888a8d63cd5525
Souad SEBAA <is_sebaa@esi.dz>
2023-07-28 15:59:16 +0200

    fixed Device.py && nm=3,nd=17,ko=7,vh=0.1,gr=100

--- Device.py
+++ Device.py
@@ -506,8 +506,8 @@ class Device:
 						local_epoch_seq = valid_validator_sig_worker_transaciton['local_total_accumulated_epochs_this_round']
 						positive_direction_validators = valid_validator_sig_worker_transaciton['positive_direction_validators']
 						negative_direction_validators = valid_validator_sig_worker_transaciton['negative_direction_validators']
-						# if len(positive_direction_validators) >= len(negative_direction_validators):
-						if True:
+						if len(positive_direction_validators) >= len(negative_direction_validators):
+						# if True:
 							# worker transaction can be used
 							valid_transactions_records_by_worker[worker_device_idx]['positive_epochs'].add(local_epoch_seq)
 							valid_transactions_records_by_worker[worker_device_idx]['all_valid_epochs'].add(local_epoch_seq)
@@ -1006,7 +1006,7 @@ class Device:
 			return sum_accu / num
 
 
-	2def global_update(self, local_update_params_potentially_to_be_used):
+	def global_update(self, local_update_params_potentially_to_be_used):
 		# filter local_params
 		local_params_by_benign_workers = []
 		for (worker_device_idx, local_params) in local_update_params_potentially_to_be_used:
@@ -1047,9 +1047,9 @@ class Device:
 			else:
 				votes[worker_device_idx]['negative_votes'] += 1
 		for worker_device_idx in votes:
-			# if not worker_device_idx in self.black_list \
-				# and votes[worker_device_idx]['positive_votes'] > votes[worker_device_idx]['negative_votes']:
-			if not worker_device_idx in self.black_list:
+			if not worker_device_idx in self.black_list \
+				and votes[worker_device_idx]['positive_votes'] > votes[worker_device_idx]['negative_votes']:
+			# if not worker_device_idx in self.black_list:
 				# print(f"---------- including wroker {worker_device_idx} in global model")
 				local_updates_params = votes[worker_device_idx]['local_updates_params']
 				if not sum_parameters:
